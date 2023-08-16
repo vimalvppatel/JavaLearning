2fce26be9b7fc817c9e3d06dce0fcb5e0471a299
Vimal Patel <vimalpatel35458@gmail.com>
2023-08-16 18:17:30 +0530

    Adding base code

---
+++ src/main/java/variable/veriablelearning.java
@@ -0,0 +1,57 @@
+package variable;
+
+public class veriablelearning {
+	
+  public static void main (String []args) {
+	  
+            // public = Access specifier, static = keyword, void = return type , Main = method name, args = array of string type 
+	  
+	  int num1 = 100; //data type, variable = value/literal data;
+	  int num2 = 200;  //data type, variable = value/literal data;
+	  int num3 = 300;   //data type, variable = value/literal data;
+	  
+	 int sum = num1 + num2 + num3;
+	 
+	   System.out.println(sum);  
+	   
+	   System.out.println("I am vimal patel");  
+	   
+	   System.out.println("Hello word");
+	   
+	   System.out.println( "first java project");
+	   
+	   System.out.println(true);
+	   
+	   System.out.println(num3);
+	   
+       System.out.println(67>15 && 90>20);
+       
+       System.out.println(10*25 - 100/2);
+       
+       System.out.println(122/100 + 500/5);
+       
+       int i = 20 ;
+       int c = ++i ;
+       System.out.println(c);
+       
+       int a = 20;
+       int b = 20;
+       System.out.println(a*b);
+       
+       
+       int y = 7;
+       int x = ++y * 10;
+       System.out.println(x);
+	   
+       char ch = 'a' ;
+       System.out.println(++ch);
+       
+       char vimal = 'D';
+       System.out.println(++vimal);
+       
+         
+      
+  }
+	
+}
+
