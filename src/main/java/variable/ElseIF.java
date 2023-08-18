237abb807970e5f1dc39bd116bea5829e9427b38
Vimal Patel <vimalpatel35458@gmail.com>
2023-08-18 15:01:31 +0530

    New program

---
+++ src/main/java/variable/ElseIF.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class ElseIF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+  int time = 24;
+	if (time < 25) {
+	System.out.println("Good Morning");
+	}
+	
+	else if (time < 13) {
+	System.out.println("Good Afternoon");
+	}
+	
+	else  {
+		System.out.println("Good Evening");
+	}
+	
+	}
+
+}
