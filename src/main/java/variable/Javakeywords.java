ca83011eb7c76f5b11d89f4d4c7d21ee85d2a485
Vimal Patel <vimalpatel35458@gmail.com>
2023-08-28 15:10:22 +0530

    New project add

--- src/main/java/variable/Javakeywords.java
+++ src/main/java/variable/Javakeywords.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class Javakeywords {	
 	
-  public static void main ( String []args) {
+  private static Scanner sc;
+
+public static void main ( String []args) {
 	  
 	System.out.println("Taking Input from the user");
 	
-	Scanner sc = new Scanner (System.in);
+	sc = new Scanner (System.in);
 	
 	 System.out.println("Enetr first number 1");
 	    int a = sc.nextInt();
@@ -17,12 +19,10 @@ public class Javakeywords {
 	  System.out.println("Enter second number 2");
 	     int b = sc.nextInt();
 	    		
-	  
+	 
 	   int Sum = a + b;
 	    
 	   System.out.println (Sum);
-	   
-	   
 	   
 	    
   }
--- src/main/java/variable/max.java
+++ src/main/java/variable/max.java
@@ -1,16 +1,14 @@
 package variable;
 
-public class WhileLoop {
+public class max {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	 int i = 0 ;
-	 while (i<=100) {
-		 System.out.println(i);
-		 i++;
 		 
-	  }		
+		int a = 100;
+		int b = 800 ;
+	System.out.println(Math.max(a, b));
+
 	}
 
 }
