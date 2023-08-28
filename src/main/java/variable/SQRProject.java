ce9937abf31dac3af2c851614c342f441386216b
Vimal Patel <vimalpatel35458@gmail.com>
2023-08-28 15:14:18 +0530

    New Project

---
+++ src/main/java/variable/SQRProject.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class SQRProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 64;
+		System.out.println(Math.sqrt(a));
+
+	}
+
+}
