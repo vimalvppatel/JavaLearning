b230106ac6cf572abf35ab2d4a425f0761292d95
Vimal Patel <vimalpatel35458@gmail.com>
2023-08-29 18:37:42 +0530

    My method

--- src/main/java/variable/ArrayProgram.java
+++ src/main/java/variable/ArrayProgram.java
@@ -5,8 +5,8 @@ public class ArrayProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-  String [] Name = {"Vimal", "Patel", "Gautam", "Patel"};
-     System.out.println(Name[0]);
+  String [] Name = {"Vimal", "Patel", "Gautam", "Patel", "Mayank", "Kanwa"}; // Array ke under bahut sare variables ho skate he
+     System.out.println(Name.length); // Execute with indexing
 
 	}
 
---
+++ src/main/java/variable/MethodClass.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class MethodClass {
+
+	public static void myMethod() {
+		// TODO Auto-generated method stub
+		System.out.println("I am vimal patel");
+}
+	public static void main (String[]args) {
+		myMethod();
+	}
+
+}
+
+
