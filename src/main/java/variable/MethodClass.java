755154fcff5c9ac976cb48d2b62efc6b1c559232
vimalvppatel <138659897+vimalvppatel@users.noreply.github.com>
2023-08-29 18:43:16 +0530

    Update MethodClass.java

--- src/main/java/variable/MethodClass.java
+++ src/main/java/variable/MethodClass.java
@@ -4,7 +4,9 @@ public class MethodClass {
 
 	public static void myMethod() {
 		// TODO Auto-generated method stub
-		System.out.println("I am vimal patel");
+		System.out.println("I am Vimal Patel"); //A method is a block of code that only runs when it is called.
+
+
 }
 	public static void main (String[]args) {
 		myMethod();
