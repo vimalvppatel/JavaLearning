45150712cd3284cf7f54b721d2abedfca98aeb52
Vimal Patel <vimalpatel35458@gmail.com>
2023-08-29 17:41:36 +0530

    Array program

---
+++ src/main/java/variable/ArrayProgram.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class ArrayProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+  String [] Name = {"Vimal", "Patel", "Gautam", "Patel"};
+     System.out.println(Name[0]);
+
+	}
+
+}
--- src/main/java/variable/ForLoop2.java
+++ src/main/java/variable/ForLoop2.java
@@ -5,15 +5,15 @@ public class ForLoop2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int i =1;
-		for (i=0;i<2;i++) {
-		System.out.println("outer:" + i);
-		
-		int j = 0;
-		for (j = 1; j<3 ; j++) {
-	    System.out.println("Inner" + j);   
-	    
-	  }
+		int i = 0;
+	for (i = 0; i < 10; i ++ ) {
+		System.out.println("Outer"+ i);
 	}
-  }
-}
+	  int j = 0;
+   for (i = 0; i<5 ; i ++) {
+	   System.out.println("Inner" + j);
+	   }
+	}
+ }
+
+
