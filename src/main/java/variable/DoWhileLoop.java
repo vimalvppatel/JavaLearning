ffbff0aa73897e4b635ff4404cb97d19e24d689d
Vimal Patel <vimalpatel35458@gmail.com>
2023-08-29 14:27:55 +0530

    Do while loop update

---
+++ src/main/java/variable/DoWhileLoop.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+   int i = 0 ;
+   
+   do {
+   System.out.println(i);
+    i ++;
+   }
+   while (i<10);
+   
+	}
+}
--- src/main/java/variable/SQRProject.java
+++ src/main/java/variable/SQRProject.java
@@ -5,7 +5,9 @@ public class SQRProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 64;
-		System.out.println(Math.sqrt(a));
+		int b =100;
+		System.out.println(Math.max(a,b));
+		System.out.println("Hello Vimal Patel");
 
 	}
 
