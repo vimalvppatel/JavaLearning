6b73d3810903aaa772d96ec64d20f8256e946934
Gaurank <gaurankzerbade5159@gmail.com>
2023-10-17 03:28:58 +0530

    new project

---
+++ src/main/java/variable/Javamethod.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Javamethod {
+
+	static void vimal () {
+		   System.out.println("I am Vimal patel");
+
+	}
+	   public static void main(String[]args) {
+		     vimal();
+		     vimal();
+	
+	 int randomNum = (int) Math.random();
+	 System.out.println(randomNum);
+		   
+	   }
+	}
---
+++ src/main/java/variable/ScannerLearn.java
@@ -0,0 +1,39 @@
+package variable;
+
+import java.util.Scanner;
+
+public class ScannerLearn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+//		System.out.println("Taking input from the user a ");
+//		 int a = sc.nextInt();
+//	
+//		 System.out.println("Taking input from user b ");
+//		 int b = sc.nextInt();
+//		
+//		 System.out.println("Sum of these 2 numbers");
+//		 System.out.println("Multiple these two numbers");
+//		
+//		 int sum = a * b;
+//		 int sum1 = a + b;
+//		 System.out.println(sum1);
+//		 System.out.println(sum);
+//		 
+	     // boolean b1 = sc.hasNextInt();
+        //System.out.println(b1);
+
+       //String str = sc.nextLine();	
+      //System.out.println(str);
+		 
+		 int x = sc.nextInt();
+		  System.out.print(x>10);
+		 
+               
+		 
+	}
+
+}
