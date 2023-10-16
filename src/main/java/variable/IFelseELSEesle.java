ae317ef6aa86272c7d85ad97f821a23e3046a052
Gaurank <gaurankzerbade5159@gmail.com>
2023-10-17 03:49:16 +0530

    scanner class prject

---
+++ src/main/java/variable/IFelseELSEesle.java
@@ -0,0 +1,30 @@
+package variable;
+
+import java.util.Scanner;
+
+public class IFelseELSEesle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		
+		if (age < 10) {
+			System.out.println("I am children");
+		}
+    
+		else if (age < 20) {
+			System.out.println("I am unmarried");
+		}
+		
+		else if (age < 30 ) {
+			System.out.println("I am mature ");
+		}
+		
+		else {
+			System.out.println("I am old man");
+		}
+	}
+
+}
--- src/main/java/variable/ScannerLearn.java
+++ src/main/java/variable/ScannerLearn.java
@@ -9,28 +9,28 @@ public class ScannerLearn {
 
 		Scanner sc = new Scanner(System.in);
 		
-//		System.out.println("Taking input from the user a ");
-//		 int a = sc.nextInt();
-//	
-//		 System.out.println("Taking input from user b ");
-//		 int b = sc.nextInt();
-//		
-//		 System.out.println("Sum of these 2 numbers");
-//		 System.out.println("Multiple these two numbers");
-//		
-//		 int sum = a * b;
-//		 int sum1 = a + b;
-//		 System.out.println(sum1);
-//		 System.out.println(sum);
-//		 
-	     // boolean b1 = sc.hasNextInt();
-        //System.out.println(b1);
-
-       //String str = sc.nextLine();	
-      //System.out.println(str);
+		System.out.println("Taking input from the user a ");
+		 int a = sc.nextInt();
+	
+		 System.out.println("Taking input from user b ");
+		 int b = sc.nextInt();
+		
+		 System.out.println("Sum of these 2 numbers");
+		 System.out.println("Multiple these two numbers");
+		
+		 int sum = a * b;
+		 int sum1 = a + b;
+		 System.out.println(sum1);
+		 System.out.println(sum);
 		 
-		 int x = sc.nextInt();
-		  System.out.print(x>10);
+	         // boolean b1 = sc.hasNextInt();
+            //System.out.println(b1);
+
+          //String str = sc.nextLine();	
+         //System.out.println(str);
+		 
+       //int x = sc.nextInt();
+      //System.out.print(x>10);
 		 
                
 		 
