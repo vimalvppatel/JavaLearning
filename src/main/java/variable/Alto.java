c44b303a37d4ead823ae14a20b751246bdd0312d
Vimal Patel <vimalpatel35458@gmail.com>
2023-10-27 19:39:15 +0530

    Interface project

---
+++ src/main/java/variable/Alto.java
@@ -0,0 +1,25 @@
+package variable;
+
+public class Alto implements Car {
+
+	@Override
+	public void Start() {
+		System.out.println("Start alto");
+		
+	}
+
+	@Override
+	public void Stop() {
+		System.out.println("Stop alto");
+		
+	}
+
+	@Override
+	public void Horn() {
+		System.out.println("Alto horn");
+		
+	}
+
+	
+
+}
