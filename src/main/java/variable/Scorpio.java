4fce8104f83bc1e93f10cc8ca49aee6add9a4f06
Vimal Patel <vimalpatel35458@gmail.com>
2023-10-27 19:41:19 +0530

    interface project

---
+++ src/main/java/variable/Scorpio.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class Scorpio implements Car{
+
+	@Override
+	public void Start() {
+		System.out.println("Start scorpio");
+		
+	}
+
+	@Override
+	public void Stop() {
+		System.out.println("Stop scorpio");
+		
+	}
+
+	@Override
+	public void Horn() {
+		System.out.println(" scorpio Horn");
+		
+	}
+
+}
